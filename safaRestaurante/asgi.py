2cf951de5ff8cc1886d65a66cc3b212c107bfa43
Alejandro <acambrollemoraza@safareyes.es>
2025-04-01 19:52:50 +0200

    Primeros Archivos

---
+++ safaRestaurante/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for safaRestaurante project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'safaRestaurante.settings')
+
+application = get_asgi_application()
